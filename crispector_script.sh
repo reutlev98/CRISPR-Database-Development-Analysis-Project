51601c5dd7bbd27865bbe8bd787460b9ed93ac50
reutlev98 <118205078+reutlev98@users.noreply.github.com>
2023-03-29 11:34:44 +0300

    Add files via upload
    crispector new script

---
+++ crispector_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash‏
+File="dataFileForCrispectorScript.csv"
+log_file="crispector_log"
+superFolfder="rhAmpSeq_data"
+cat $File |while read line
+do
+	#take the first col 
+	EXP=$(echo $line | awk '{print $1}');
+	Duplicate=$(echo $line | awk '{print $2}');
+	CONF=$(echo $line | awk  '{print $3}');
+	echo $EXP $MOCK $CONF
+	crispector -t_r1 $superFolfder/$EXP/$Duplicate/$EXP"-Cr-"$Duplicate".r1.fastq.gz" -t_r2 $superFolfder/$EXP/$Duplicate/$EXP"-Cr-"$Duplicate".r2.fastq.gz" -m_r1 $superFolfder/$EXP/$Duplicate/$EXP"-Mock-"$Duplicate".r1.fastq.gz" -m_r2 $superFolfder/$EXP/$Duplicate/$EXP"-Mock-"$Duplicate".r2.fastq.gz" -c rhamseq_config_file/$CONF |& tee -a $log_file
+done
+
