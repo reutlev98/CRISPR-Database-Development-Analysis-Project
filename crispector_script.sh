9dc0a7077b193ed5e05e90cedf5eda41dead8bc2
reutlev98 <118205078+reutlev98@users.noreply.github.com>
2023-04-19 11:17:38 +0300

    Update crispector_script.sh
    add output folder

--- crispector_script.sh
+++ crispector_script.sh
@@ -9,6 +9,6 @@ do
 	Duplicate=$(echo $line | awk '{print $2}');
 	CONF=$(echo $line | awk  '{print $3}');
 	echo $EXP $MOCK $CONF
-	crispector -t_r1 $superFolfder/$EXP/$Duplicate/$EXP"-Cr-"$Duplicate".r1.fastq.gz" -t_r2 $superFolfder/$EXP/$Duplicate/$EXP"-Cr-"$Duplicate".r2.fastq.gz" -m_r1 $superFolfder/$EXP/$Duplicate/$EXP"-Mock-"$Duplicate".r1.fastq.gz" -m_r2 $superFolfder/$EXP/$Duplicate/$EXP"-Mock-"$Duplicate".r2.fastq.gz" -c rhamseq_config_file/$CONF |& tee -a $log_file
+	crispector -t_r1 $superFolfder/$EXP/$Duplicate/$EXP"-Cr-"$Duplicate".r1.fastq.gz" -t_r2 $superFolfder/$EXP/$Duplicate/$EXP"-Cr-"$Duplicate".r2.fastq.gz" -m_r1 $superFolfder/$EXP/$Duplicate/$EXP"-Mock-"$Duplicate".r1.fastq.gz" -m_r2 $superFolfder/$EXP/$Duplicate/$EXP"-Mock-"$Duplicate".r2.fastq.gz" -c rhamseq_config_file/$CONF -o $EXP"_"$Duplicate|& tee -a $log_file
 done
 
